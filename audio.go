efb7f71f3f9deae920424c7837362914e6436b33
zlasd <zlasd@hotmail.com>
2023-07-11 23:01:33 +0800

    feat: implement audio interface

--- audio.go
+++ audio.go
@@ -1,5 +1,12 @@
 package projectm
 
+/*
+#cgo CFLAGS: -I${SRCDIR}/include
+#cgo LDFLAGS: -lprojectM-4
+#include <projectM-4/projectM.h>
+*/
+import "C"
+
 const (
 	PROJECTM_MONO   = 1
 	PROJECTM_STEREO = 2
@@ -14,14 +21,20 @@ const (
 
 // PCMGetMaxChannels returns the maximum number of audio samples that can be stored.
 func PCMGetMaxChannels() uint32 {
-	return 0
+	return uint32(C.projectm_pcm_get_max_samples())
 }
 
 // PCMAddFloat adds 32-bit floating-point audio samples.
-func (h *Handle) PCMAddFloat(samples []float32, channels int) {}
+func (h *Handle) PCMAddFloat(samples []float32, channels int) {
+	C.projectm_pcm_add_float(C.projectm_handle(h.handle), samples, len(samples), C.projectm_channels(channels))
+}
 
 // PCMAddInt16 adds 16-bit integer audio samples.
-func (h *Handle) PCMAddInt16(samples []int16, channels int) {}
+func (h *Handle) PCMAddInt16(samples []int16, channels int) {
+	C.projectm_pcm_add_int16(C.projectm_handle(h.handle), samples, len(samples), C.projectm_channels(channels))
+}
 
 // PCMAddUint8 adds 8-bit unsigned integer audio samples.
-func (h *Handle) PCMAddUint8(samples []uint8, channels int) {}
+func (h *Handle) PCMAddUint8(samples []uint8, channels int) {
+	C.projectm_pcm_add_uint8(C.projectm_handle(h.handle), samples, len(samples), C.projectm_channels(channels))
+}
